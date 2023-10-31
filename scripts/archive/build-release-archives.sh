3b1dd19d72c100d688c2e35bb20ac44367b94aa1
dkijania <dariusz@o1labs.org>
2023-10-31 20:29:42 +0100

    package generate random blocks

--- scripts/archive/build-release-archives.sh
+++ scripts/archive/build-release-archives.sh
@@ -60,6 +60,7 @@ ls
 cp ./default/src/app/archive/archive.exe "${BUILD_DIR}/usr/local/bin/mina-archive"
 cp ./default/src/app/archive_blocks/archive_blocks.exe "${BUILD_DIR}/usr/local/bin/mina-archive-blocks"
 cp ./default/src/app/extract_blocks/extract_blocks.exe "${BUILD_DIR}/usr/local/bin/mina-extract-blocks"
+cp ./default/src/app/generate_random_blocks/generate_random_blocks.exe "${BUILD_DIR}/usr/local/bin/mina-generate-random-blocks"
 cp ./default/src/app/missing_blocks_auditor/missing_blocks_auditor.exe "${BUILD_DIR}/usr/local/bin/mina-missing-blocks-auditor"
 cp ./default/src/app/replayer/replayer.exe "${BUILD_DIR}/usr/local/bin/mina-replayer"
 cp ./default/src/app/swap_bad_balances/swap_bad_balances.exe "${BUILD_DIR}/usr/local/bin/mina-swap-bad-balances"
