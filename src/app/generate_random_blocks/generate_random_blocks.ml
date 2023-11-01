c8c3ffe16a73771335395fb8376457951081ddda
dkijania <dariusz@o1labs.org>
2023-11-01 23:32:49 +0100

    improve generate_random_blocks

--- src/app/generate_random_blocks/generate_random_blocks.ml
+++ src/app/generate_random_blocks/generate_random_blocks.ml
@@ -1,8 +1,9 @@
 open Frontier_base
-open Core_kernel
 open Signature_lib
 open Mina_base
 open Mina_block
+open Core
+open Async
 
 let () =
   let open Core_kernel in
@@ -34,10 +35,15 @@ let gen_breadcrumb =
   Breadcrumb.For_tests.gen ~logger ~precomputed_values ~verifier
     ?trust_system:None ~accounts_with_secret_keys
 
+let gen_breadcrumb_seq =
+  Breadcrumb.For_tests.gen_seq ~logger ~precomputed_values ~verifier
+    ?trust_system:None ~accounts_with_secret_keys
+
 module Transfer = Ledger_transfer.Make (Ledger) (Ledger)
 
 let create_frontier () =
   let open Core in
+  let open Deferred.Let_syntax in
   let epoch_ledger_location =
     Filename.temp_dir_name ^/ "epoch_ledger"
     ^ (Uuid_unix.create () |> Uuid.to_string)
@@ -58,6 +64,32 @@ let create_frontier () =
          ~dest:(Ledger.create ~depth:ledger_depth ()) )
   in
   Protocol_version.(set_current zero) ;
+ 
+  let create_ledger_as_list ledger =
+    let%map accounts = Ledger.to_list ledger in
+    List.map accounts ~f:(fun acc ->
+        Genesis_ledger_helper.Accounts.Single.of_account acc None )
+
+  in
+  let%bind genesis_ledger_as_list = create_ledger_as_list root_ledger
+  in
+
+  let snarked_ledger_hash =
+    root_ledger |> Mina_base.Ledger.merkle_root
+    |> Mina_base.Frozen_ledger_hash.of_ledger_hash |> Mina_base.Frozen_ledger_hash.to_decimal_string
+  in
+  let genesis_ledger : Runtime_config.Ledger.t =
+    { base = Accounts genesis_ledger_as_list
+    ; num_accounts = Some (Ledger.num_accounts root_ledger)
+    ; balances = []
+    ; hash = Some snarked_ledger_hash
+    ; name = Some "test"
+    ; add_genesis_winner = Some true
+    }
+  in
+  Core_kernel.printf !"%{Yojson.Safe}\n"
+  (Runtime_config.Ledger.to_yojson genesis_ledger) ;
+
   let root_data =
     let open Root_data in
     { transition =
@@ -77,11 +109,11 @@ let create_frontier () =
   let persistent_root_instance =
     Persistent_root.create_instance_exn persistent_root
   in
-  Full_frontier.create ~logger ~root_data
+  Deferred.return (Full_frontier.create ~logger ~root_data
     ~root_ledger:(Ledger.Any_ledger.cast (module Ledger) root_ledger)
     ~consensus_local_state ~max_length ~precomputed_values
     ~time_controller:(Block_time.Controller.basic ~logger)
-    ~persistent_root_instance
+    ~persistent_root_instance)
 
 let clean_up_persistent_root ~frontier =
   let persistent_root_instance =
@@ -96,9 +128,10 @@ let clean_up_persistent_root ~frontier =
    * include all types of transactions
    * etc.
 *)
-let f make_breadcrumb =
+let single make_breadcrumb =
   Async.Thread_safe.block_on_async_exn (fun () ->
-      let frontier = create_frontier () in
+      let open Deferred.Let_syntax in
+      let%bind frontier = create_frontier () in
       let root = Full_frontier.root frontier in
       let open Async_kernel.Deferred.Let_syntax in
       let%map breadcrumb = make_breadcrumb root in
@@ -112,11 +145,59 @@ let f make_breadcrumb =
         Mina_block.Precomputed.of_block ~scheduled_time
           (Breadcrumb.block breadcrumb)
       in
-      Core_kernel.eprintf !"Randomly generated block, sexp:\n" ;
-      Core_kernel.printf !"%{sexp:Mina_block.Precomputed.t}\n" precomputed ;
-      Core_kernel.eprintf !"Randomly generated block, json:\n" ;
       Core_kernel.printf !"%{Yojson.Safe}\n"
         (Mina_block.Precomputed.to_yojson precomputed) ;
       clean_up_persistent_root ~frontier )
 
-let () = Core_kernel.Quickcheck.test gen_breadcrumb ~trials:1 ~f
+      let add_breadcrumb frontier breadcrumb =
+        let diffs = Full_frontier.calculate_diffs frontier breadcrumb in
+        ignore
+          ( Full_frontier.apply_diffs frontier diffs ~has_long_catchup_job:false
+              ~enable_epoch_ledger_sync:`Disabled
+            : [ `New_root_and_diffs_with_mutants of
+                Root_identifier.t option * Diff.Full.With_mutant.t list ] )
+  
+let f make_seq =
+          Async.Thread_safe.block_on_async_exn (fun () ->
+            let open Deferred.Let_syntax in
+            let%bind frontier = create_frontier () in
+              let root = Full_frontier.root frontier in
+              let open Async_kernel.Deferred.Let_syntax in
+              let%map seq = make_seq root in
+              List.iter seq ~f:(fun breadcrumb ->
+                let block = Breadcrumb.block breadcrumb in
+                let scheduled_time =
+                  Mina_block.(Header.protocol_state @@ header block)
+                  |> Mina_state.Protocol_state.blockchain_state
+                  |> Mina_state.Blockchain_state.timestamp
+                in
+                let precomputed =
+                  Mina_block.Precomputed.of_block ~scheduled_time
+                    (Breadcrumb.block breadcrumb)
+                in
+                Core_kernel.printf !"RandomBlock: %{Yojson.Safe}\n"
+                  (Mina_block.Precomputed.to_yojson precomputed) ;
+                add_breadcrumb frontier breadcrumb ;
+              );  
+              clean_up_persistent_root ~frontier ) 
+
+      
+let () =
+  Command.(
+    run
+      (let open Let_syntax in
+      Command.basic ~summary:"Migrate balances table to extended balances table"
+        (let%map_open block_count =
+           Param.flag "--block-count"
+             ~doc:
+               "Number of blocks to generate"
+             Param.(required int)
+         in
+         fun () -> 
+            if block_count <= 1 then
+              Core_kernel.Quickcheck.test gen_breadcrumb ~trials:1 ~f:single
+            else 
+              Core_kernel.Quickcheck.test (gen_breadcrumb_seq block_count) ~trials:1 ~f 
+        )
+      )
+  )
