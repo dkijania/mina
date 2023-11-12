8ca2daaa1156ab43017819246a92e308837a27fd
dkijania <dariusz@o1labs.org>
2023-11-12 17:54:37 +0100

    add sudo

--- buildkite/scripts/replayer-test.sh
+++ buildkite/scripts/replayer-test.sh
@@ -5,26 +5,26 @@ TEST_DIR=/workdir/src/app/replayer/
 set -eox pipefail
 
 echo "Updating apt, installing packages"
-sudo apt-get update
+apt-get update
 # Don't prompt for answers during apt-get install
 export DEBIAN_FRONTEND=noninteractive
 
 # time zone = US Pacific
 /bin/echo -e "12\n10" | sudo apt-get install -y tzdata
-sudo apt-get install -y git apt-transport-https ca-certificates curl wget
+apt-get install -y git apt-transport-https ca-certificates curl wget
 
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
 
 echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | sudo tee /etc/apt/sources.list.d/mina.list
-sudo apt-get update
+apt-get update
 
 echo "Installing archive node package: mina-archive=${MINA_DEB_VERSION}"
-sudo apt-get install --allow-downgrades -y mina-archive=${MINA_DEB_VERSION}
+apt-get install --allow-downgrades -y mina-archive=${MINA_DEB_VERSION}
 
 echo "Starting Postgresql service"
-sudo service postgresql start
+service postgresql start
 
 echo "Postgres is up - executing command"
 
