66e98c599e52d0c3ef9891b0697eb9e7a2240f6a
dkijania <dariusz@o1labs.org>
2023-11-16 13:52:02 +0100

    build test suite apps and as example run single node tests on build apps

--- buildkite/scripts/single-node-tests.sh
+++ buildkite/scripts/single-node-tests.sh
@@ -5,27 +5,17 @@ set -eo pipefail
 # Don't prompt for answers during apt-get install
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get update
-sudo apt-get install -y git apt-transport-https ca-certificates tzdata curl
-
-TESTNET_NAME="berkeley"
+apt-get update
+apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
 
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
 
-#test executive dependency
-echo "deb [trusted=yes] https://apt.releases.hashicorp.com $MINA_DEB_CODENAME main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update
-sudo apt-get install -y "terraform"
+echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | tee /etc/apt/sources.list.d/mina.list
+apt-get update
 
-echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
-echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | sudo tee /etc/apt/sources.list.d/mina.list
-sudo apt-get update
-sudo apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}" "mina-test-executive=${MINA_DEB_VERSION}"
+echo "Installing mina test suite package: mina-test-suite=${MINA_DEB_VERSION}"
+apt-get install --allow-downgrades -y mina-test-suite=${MINA_DEB_VERSION} mina-berkeley=${MINA_DEB_VERSION}
 
-mkdir -p _build/default/src/app/cli/src
-sudo cp /usr/local/bin/mina _build/default/src/app/cli/src/mina.exe
-
-echo "--- Run tests"
-./mina-command-line-tests.exe
+mina-command-line-tests --mina-path mina-daemon
