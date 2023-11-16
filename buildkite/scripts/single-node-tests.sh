531c620a6af4541272041d34f55044f651f39326
dkijania <dariusz@o1labs.org>
2023-11-16 19:59:42 +0100

    more sudo

--- buildkite/scripts/single-node-tests.sh
+++ buildkite/scripts/single-node-tests.sh
@@ -13,9 +13,9 @@ git config --global --add safe.directory /workdir
 source buildkite/scripts/export-git-env-vars.sh
 
 sudo echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | sudo tee /etc/apt/sources.list.d/mina.list
-apt-get update
+sudo apt-get update
 
 echo "Installing mina test suite package: mina-test-suite=${MINA_DEB_VERSION}"
-apt-get install --allow-downgrades -y mina-test-suite=${MINA_DEB_VERSION} mina-berkeley=${MINA_DEB_VERSION}
+sudo apt-get install --allow-downgrades -y mina-test-suite=${MINA_DEB_VERSION} mina-berkeley=${MINA_DEB_VERSION}
 
 mina-command-line-tests --mina-path mina-daemon
