19878f79388bd7e5b137fe74bd6cdabdcd54ca71
Dariusz Kijania <dariusz.kijania@gmail.com>
2023-11-16 19:20:00 +0100

    Update single-node-tests.sh

--- buildkite/scripts/single-node-tests.sh
+++ buildkite/scripts/single-node-tests.sh
@@ -5,14 +5,14 @@ set -eo pipefail
 # Don't prompt for answers during apt-get install
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
+sudo apt-get update
+sudo apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
 
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
 
-echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | tee /etc/apt/sources.list.d/mina.list
+sudo echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | sudo tee /etc/apt/sources.list.d/mina.list
 apt-get update
 
 echo "Installing mina test suite package: mina-test-suite=${MINA_DEB_VERSION}"
